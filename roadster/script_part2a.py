3b91b20a35dc6af8ce4cec58ba2a49ada8897393
Fr0dr1k <77593162+Fr0dr1k@users.noreply.github.com>
2023-09-26 22:49:14 +0200

    By Fredrik

--- roadster/script_part2a.py
+++ roadster/script_part2a.py
@@ -2,4 +2,6 @@ import roadster as r
 
 if __name__ == '__main__':
     anna = r.load_route("speed_anna.npz")
-    print(r.time_to_destination(anna[0][-1],"speed_anna.npz",10000000))
+    elsa = r.load_route("speed_elsa.npz")
+    print("Anna time",r.time_to_destination(anna[0][-1],"speed_anna.npz",10000000))
+    print("Elsa time", r.time_to_destination(elsa[0][-1], "speed_elsa.npz", 10000000))
